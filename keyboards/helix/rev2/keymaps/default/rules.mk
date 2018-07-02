6ccfdab28074f6d49459e0b2f168ada69648ebf9
Noritaka Kamiya <noritakakamiya@gmail.com>
2018-07-02 21:57:55 +0900

    Customize keys

--- keyboards/helix/rev2/keymaps/default/rules.mk
+++ keyboards/helix/rev2/keymaps/default/rules.mk
@@ -29,10 +29,10 @@ endef
 # Helix keyboard customize
 # you can edit follows 7 Variables
 #  jp: 以下の7つの変数を必要に応じて編集します。
-HELIX_ROWS = 5              # Helix Rows is 4 or 5
-OLED_ENABLE = no            # OLED_ENABLE
+HELIX_ROWS = 4              # Helix Rows is 4 or 5
+OLED_ENABLE = yes            # OLED_ENABLE
 LOCAL_GLCDFONT = no         # use each keymaps "helixfont.h" insted of "common/glcdfont.c"
-LED_BACK_ENABLE = no        # LED backlight (Enable WS2812 RGB underlight.)
+LED_BACK_ENABLE = yes        # LED backlight (Enable WS2812 RGB underlight.)
 LED_UNDERGLOW_ENABLE = no   # LED underglow (Enable WS2812 RGB underlight.)
 LED_ANIMATIONS = yes        # LED animations
 IOS_DEVICE_ENABLE = no      # connect to IOS device (iPad,iPhone)
@@ -71,8 +71,8 @@ endif
 
 # Uncomment these for checking
 #   jp: コンパイル時にカスタマイズの状態を表示したい時はコメントをはずします。
-# $(eval $(call HELIX_CUSTOMISE_MSG))
-# $(info )
+$(eval $(call HELIX_CUSTOMISE_MSG))
+$(info )
 
 ifneq ($(strip $(HELIX_ROWS)), 4)
   ifneq ($(strip $(HELIX_ROWS)), 5)
